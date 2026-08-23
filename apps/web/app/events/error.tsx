a968eb6065c579c39c366c51170bdc7f2c3bd07b
AbhishekRajoria <Abhishekrajoria24@gmail.com>
2026-08-23 20:20:41 +0530

    feat(web): add loading skeleton and error boundary for events

--- apps/web/app/events/error.tsx
+++ apps/web/app/events/error.tsx
@@ -1,5 +1,37 @@
 "use client";
 
-export default function Events() {
-  return <div>Something went wrong!</div>;
+export default function Events({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+  }) {
+
+  return (
+    <main className="max-w-5xl  mx-auto px-6 py-10">
+      <h1 className="text-2xl font-bold tracking-tight">Event</h1>
+
+      <table className="mt-6 w-full border-collapse text-left text-sm">
+        <thead className="">
+          <tr className="border-b text-xs uppercase tracking-wide text-gray-500">
+            <th className="px-4 py-3 font-medium ">Event Name</th>
+            <th className="px-4 py-3 font-medium ">Status</th>
+            <th className="px-4 py-3 font-medium ">Channel</th>
+            <th className="px-4 py-3 font-medium ">Received At</th>
+          </tr>
+        </thead>
+      </table>
+      <div className="flex flex-col items-center py-10 bg-gray-100 font-bold ">
+        <span>Error Loading Data</span>
+        <span>{error.message}</span>
+        <button
+          onClick={() => reset()}
+          className="bg-gray-400 hover:bg-gray-300 hover:px-1.5  w-fit px-2 py-1 rounded-sm"
+        >
+          Retry
+        </button>
+      </div>
+    </main>
+  );
 }
--- apps/web/app/events/loading.tsx
+++ apps/web/app/events/loading.tsx
@@ -1,5 +1,36 @@
-"use client";
-
 export default function Events() {
-  return <div>Something went wrong!</div>;
+  return (
+    <main className="max-w-5xl  mx-auto px-6 py-10">
+      <h1 className="text-2xl font-bold tracking-tight">Event</h1>
+
+      <table className="mt-6 w-full border-collapse text-left text-sm">
+        <thead className="">
+          <tr className="border-b text-xs uppercase tracking-wide text-gray-500">
+            <th className="px-4 py-3 font-medium">Event Name</th>
+            <th className="px-4 py-3 font-medium">Status</th>
+            <th className="px-4 py-3 font-medium">Channel</th>
+            <th className="px-4 py-3 font-medium">Received At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <tr
+              key={index}
+              className="border-b bg-gray-100 border-gray-300 hover:bg-gray-50 animate-pulse "
+            >
+              <td className="px-4 py-3 font-medium"></td>
+
+              <td className="px-4 py-3 text-sm text-gray-500">
+                <span className="inline-flex rounded-full px-2.5 py-0.5 text-xs font-medium"></span>
+              </td>
+
+              <td className="px-4 py-3 text-sm capitalize text-gray-500"></td>
+
+              <td className="text-xs"></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 }
